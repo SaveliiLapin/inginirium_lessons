4edb58450e2a9c7387931548ed59cca8bf32d223
SaveliiLapin <92331852+SaveliiLapin@users.noreply.github.com>
2022-05-29 12:59:34 +0400

    Update lesson_13_1.py

--- lesson_13_1.py
+++ lesson_13_1.py
@@ -1,1 +1,16 @@
+# Задание 1
+#
+# Напишите программу, которая рисует на холсте размера 400х400 “шахматное” поле
+# (8 на 8 клеток) и делает это оптимально (использовать цикл)
 
+import tkinter as tk
+
+win = tk.Tk()
+canvas = tk.Canvas(win, bg='white', width=400, height=400)
+
+for i in range(50, 400, 50):
+    canvas.create_line((i, 0), (i, 400), fill='black')
+    canvas.create_line((0, i), (400, i), fill='black')
+
+canvas.pack()
+win.mainloop()
